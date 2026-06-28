affeee60550f6c486a318f9cfdbcac6ba6cae12e
Mark Cabrera <mark.a.cabrera@accenture.com>
2026-06-28 17:37:25 +0800

    Add Spring Boot project skeleton with Web and WebSocket

---
+++ src/main/java/com/sprintrace/SprintraceApplication.java
@@ -0,0 +1,13 @@
+package com.sprintrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprintrace/SprintraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
